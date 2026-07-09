3e43cb80a1cf1b28e2eb4ac3318ae50251847a01
Joaquin Macedo <joaquinmacedosound@gmail.com>
2026-07-09 02:42:12 -0300

    Delete Contact.jsx

--- Contact.jsx
+++
@@ -1,7 +0,0 @@
-export default function PlaceholderBlock({ icon: Icon, className = '', iconSize = 28 }) {
-  return (
-    <div className={`placeholder-block ${className}`}>
-      {Icon && <Icon size={iconSize} strokeWidth={1.25} />}
-    </div>
-  );
-}
